b80a045e7cb9a2d884f5446141c9b70a319af9b0
Carsten Thiel <thiel@sub.uni-goettingen.de>
2014-05-20 15:46:30 +0200

    Auto-Create User based on Shibboleth session Auto-Sync User groups from Shibboleth to Liferay Added configuration options to Liferay-Backend Compatible to Liferay 6.2.1 (i.e. 6.2 CE GA2)

--- docroot/WEB-INF/src/com/liferay/shibboleth/util/ShibbolethUtil.java
+++ docroot/WEB-INF/src/com/liferay/shibboleth/util/ShibbolethUtil.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * Copyright (c) 2014 Georg-August-Universität Göttingen
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
